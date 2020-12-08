65fc545871294fb31c4bc02328a849cef6066a91
ralfred18404 <72418400+ralfred18404@users.noreply.github.com>
2020-12-08 19:04:25 +0200

    Update Taxi24ApplicationTests.java

--- src/test/java/com/taxi24/Taxi24ApplicationTests.java
+++ src/test/java/com/taxi24/Taxi24ApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Taxi24ApplicationTests {
+public class Taxi24ApplicationTests {
 
 	@Test
 	void contextLoads() {
